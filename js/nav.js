ee4b79cba5ef0a900e090e3fa70db15145746e6f
fret2buzz <fret2buzz@hotmail.com>
2023-02-15 23:01:10 +0200

    develop

--- js/nav.js
+++ js/nav.js
@@ -21,6 +21,9 @@ function NavControl(options) {
 
 NavControl.prototype.init = function () {
     if (this.el) {
+        let breakpointType = typeof this.settings.breakpoint === 'number';
+        let breakpointWidth = window.innerWidth >= this.settings.breakpoint;
+
         let lastToFocus = this.el.querySelectorAll('a, button');
         let index = lastToFocus.length - 1;
 
@@ -33,36 +36,66 @@ NavControl.prototype.init = function () {
         this.el.addEventListener('click', (e) => {
             this.target = e.target;
 
-            if (window.innerWidth >= this.settings.breakpoint) {
+            if (breakpointType && breakpointWidth) {
                 //desktop
-                let hasSubnav = this.target.getAttribute('aria-haspopup') == 'true';
-
-                if (!hasSubnav) {
-                    return false;
-                }
-
                 e.preventDefault();
-                this.parentElement = this.target.parentElement;
-                this.hiddenArea = this.parentElement.querySelector('[aria-hidden]');
+                this.parentContainer = this.findParent(this.target, 'main');
+                this.button = this.parentContainer.querySelector('[aria-haspopup]');
+                this.hiddenArea = this.parentContainer.querySelector('[aria-hidden]');
 
-                if (!this.parentElement.classList.contains(this.classNameActive)) {
-                    this.expand();
-                } else {
-                    this.collapse();
+                switch (true) {
+                    case this.target.getAttribute('aria-haspopup') === 'true':
+                        if (!this.parentContainer.classList.contains(this.classNameActive)) {
+                            this.expand();
+                        } else {
+                            this.collapse();
+                        }
+                        break;
+
+                    case Object.keys(this.target.dataset).includes('close'):
+                        this.collapse();
+                        this.button.focus();
+
+                        break;
+
+                    default:
+                        break;
                 }
-
             } else {
                 //mobile
+            }
+        });
+
+        this.el.addEventListener('keydown', (e) => {
+            if (breakpointType && breakpointWidth) {
+                let key = e.which;
+                if (key === 27) {
+                    // escape
+                    e.preventDefault();
+                    this.collapse();
+                    this.button.focus();
+                }
             }
         });
     }
 };
 
+NavControl.prototype.findParent = function (element, type) {
+    let end = element.parentElement.classList.contains(this.settings.SELECTOR_NAV);
+    if (Object.keys(element.dataset).includes(type)) {
+        return element;
+    } else if (element.parentElement && !end) {
+        return this.findParent(element.parentElement, type);
+    } else {
+        return false;
+    }
+}
+
 NavControl.prototype.expand = function() {
     this.hiddenArea.setAttribute('aria-hidden', 'false');
-    this.target.setAttribute('aria-expanded', 'true');
-    this.parentElement.classList.add(this.classNameActive);
-    if (this.activeElement && this.parentElement !== this.activeElement) {
+    this.button.setAttribute('aria-expanded', 'true');
+    this.parentContainer.classList.add(this.classNameActive);
+    if (this.activeElement && this.parentContainer !== this.activeElement) {
         this.collapse();
     }
     this.activeElement = document.querySelector(this.settings.SELECTOR_NAV + ' .' + this.classNameActive);
@@ -70,7 +103,7 @@ NavControl.prototype.expand = function() {
 
 NavControl.prototype.collapse = function() {
     this.hiddenArea.setAttribute('aria-hidden', 'true');
-    this.target.setAttribute('aria-expanded', 'false');
+    this.button.setAttribute('aria-expanded', 'false');
     this.activeElement.classList.remove(this.classNameActive);
     this.activeElement = null;
 }
